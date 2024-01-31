bb18ebc8365a81dd83304531148888c522bf85c3
arvincf <faustinoarvinclyde18@gmail.com>
2024-02-01 00:41:30 +0800

    Update coffeebeans.blade.php

--- resources/views/userpage/rawmat/coffeebeans.blade.php
+++ resources/views/userpage/rawmat/coffeebeans.blade.php
@@ -12,7 +12,23 @@
         <main class="main-container">
             @include('components.header')
             <h1>Stored Beans</h1>
-            
+            <div class="card">
+                <div class="card-body">
+                    <form class="form-inline d-flex">
+                        <input id="searchInput" name="name" class="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search for ...">
+                        <button class="btn btn-primary" id="searchApplicantBtn"><i class="bi bi-search"></i></button>
+                    </form>
+                    <br>
+                    <table class="table">
+                        <thead>
+                            <th>User Type</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Age</th>
+                            <th>Address</th>
+                            <th>Contact</th>
+                            <th>Actions</th>
+                        </thead>
         </main>
     </div>
 
