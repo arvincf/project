45d6ae0f64af2e2ddf6b9a5bcf35c272135e26de
arvincf <faustinoarvinclyde18@gmail.com>
2024-02-26 00:49:04 +0800

    profileUi

--- resources/views/userpage/Profile.blade.php
+++ resources/views/userpage/Profile.blade.php
@@ -7,11 +7,26 @@
 
 <body>
     <div class="wrapper">
-        @include('components.header')
+        @include('components.sidebar')
+        @include('components.dropdown')
         <main class="main-container">
-            @include('components.sidebar')
-            @include('components.dropdown')
-            @include('components.profile')
+            @include('components.header')
+            
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="panel profile">
+                        <h3 class="text-dark mb-4">Profile</h3>
+                        <div class="jumbotron text-center bg-red">
+                            <img class="img-circle img-size-2" src="{{ asset('assets/img/no_image.png')}}" alt="">
+                            
+                        </div>
+                        <ul class="nav nav-pills nav-stacked">
+                            <li><a href="{{ route(strtolower(auth()->user()->type) . '.setting') }}"> <i class="bi bi-pencil-square"></i> Edit profile</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
         </main>
     </div>
 
