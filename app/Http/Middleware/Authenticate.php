d000674c827678b8c6db2ac37beb88594db4cb09
arvincf <faustinoarvinclyde18@gmail.com>
2024-05-09 14:13:42 +0800

    authenticate

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -8,7 +8,8 @@ use Illuminate\Http\Request;
 class Authenticate extends Middleware
 {
     protected function redirectTo(Request $request): ?string
-    {
-        return $request->expectsJson() ? null : route('login');
-    }
+{
+    return $request->expectsJson() ? null : route('login');
+}
+
 }
